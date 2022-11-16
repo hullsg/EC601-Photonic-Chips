09abe0b8b137aa965f7275fb1010fdfacb7e2bb5
Shannon Hull <113312870+hullsg@users.noreply.github.com>
2022-11-15 23:23:13 -0500

    Add files via upload

---
+++ Ring Resonator Working.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+from simphony.libraries import siepic
+from simphony.simulators import SweepSimulator
+
+gc_input = siepic.GratingCoupler()
+
+#establishing waveguide between coupler and ring resonator
+wg_1 = siepic.Waveguide()
+
+
+#establishing first half-ring (gap = 30 nm, r = 10 microns)
+halfring1 = siepic.HalfRing()
+
+#establing second half-ring (gap = 30 nm, r = 10 microns)
+halfring2 = siepic.HalfRing()
+
+#establishing terminator to remove back-scattering effects
+terminator = siepic.Terminator()
+
+#renaming pins of each half-ring
+halfring1.rename_pins("pass", "midb", "in", "midt")
+halfring2.rename_pins("out", "midt", "term", "midb")
+
+#connects the two half-rings to make a complete ring
+#connects terminator to output of ring resonator
+halfring1.interface(halfring2)
+halfring2["term"].connect(terminator)
+
+#connecting grating coupler to waveguide
+wg_1.connect(gc_input["pin1"])
+#connecting ring to waveguide
+wg_1.connect(halfring1["pass"])
+
+simulator = SweepSimulator(1500e-9, 1600e-9)
+simulator.multiconnect(gc_input, halfring2["out"])
+
+f, p = simulator.simulate()
+plt.plot(f, p)
+plt.title("Ring Resonator")
+plt.tight_layout()
+plt.show()
