e82dc8d0a588b6896517af2a7fbabb9d94583ce9
Shannon Hull <113312870+hullsg@users.noreply.github.com>
2022-12-01 15:17:44 -0500

    Update Ring Resonator Working.py

--- Ring Resonator Working.py
+++ Ring Resonator Working.py
@@ -1,9 +1,10 @@
-#Simulates a circuit consisting of  grating coupler receiving input light that propogates through a ring resonator
+#Simulates circuit consisting of grating coupler recieving input light that is propogated through a ring resonator
 
 import matplotlib.pyplot as plt
 from simphony.libraries import siepic
 from simphony.simulators import SweepSimulator
 
+#establishing grating coupler to recieve input
 gc_input = siepic.GratingCoupler()
 
 #establishing waveguide between coupler and ring resonator
@@ -11,10 +12,14 @@ wg_1 = siepic.Waveguide()
 
 
 #establishing first half-ring (gap = 30 nm, r = 10 microns)
-halfring1 = siepic.HalfRing()
+halfring1 = siepic.HalfRing(radius = 1e-5)
+#halfring1 = siepic.HalfRing(radius = 2e-5)
+#halfring1 = siepic.HalfRing(radius=3e-5)
 
 #establing second half-ring (gap = 30 nm, r = 10 microns)
-halfring2 = siepic.HalfRing()
+halfring2 = siepic.HalfRing(radius=1e-5)
+#halfring2 = siepic.HalfRing(radius=2e-5)
+#halfring2 = siepic.HalfRing(radius=3e-5)
 
 #establishing terminator to remove back-scattering effects
 terminator = siepic.Terminator()
@@ -31,10 +36,10 @@ halfring2["term"].connect(terminator)
 #connecting grating coupler to waveguide
 wg_1.connect(gc_input["pin1"])
 #connecting ring to waveguide
-wg_1.connect(halfring1["pass"])
+wg_1.connect(halfring1["in"])  #was "pass"
 
-simulator = SweepSimulator(1500e-9, 1600e-9)
-simulator.multiconnect(gc_input, halfring2["out"])
+simulator = SweepSimulator(1.90e14, 1.957e14)  #frequency range in Hz, based on laser being 1.957e14 Hz
+simulator.multiconnect(gc_input, halfring2["out"]) #was "out"
 
 f, p = simulator.simulate()
 plt.plot(f, p)
